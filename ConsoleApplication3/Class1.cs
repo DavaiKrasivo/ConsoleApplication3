23c20f6f6f5f7b87d2245ad45ad3e9d5fd5b9034
DavaiKrasivo <89225651308ia@gmail.com>
2017-04-29 16:07:23 +0500

    Начало
    Все что есть

---
+++ ConsoleApplication3/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Vertex
+    {
+        public int Number;
+        public bool flag = false;
+        public int CountAdjVer;
+        public int[] AdjVer;
+        public Vertex(int num,int[] adj)
+        {
+            Number = num;
+            AdjVer = adj;
+        }
+        public int GetMin()
+        {
+            return AdjVer[0];
+        }
+        private int[] SortArray(int[] Arr)
+        {
+            int temp = Arr[0];
+            for (int i = 1;i < Arr.Length; i ++)
+            {
+                if (tem)
+            }
+        }
+    }
+}
---
+++ ConsoleApplication3/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Edge
+    {
+        public Vertex a, b;
+        public int length;
+        public Edge(Vertex A,Vertex B, int len)
+        {
+            a = A;
+            b = B;
+            length = len;
+        }
+
+    }
+}
